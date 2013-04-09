81209ab6e74cf1ccab966464823fe9150115dbf2
yfyang <yfyang@iflytek.com>
2013-04-09 17:35:28 +0800

    first commit

---
+++ search_maven.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#_*_ coding:UTF-8 _*_
+# 
+# Copyright 2012-2013 by yfyang. All Rights Reserved.
+
+import json
+import urllib
+import urllib2
+
+import alfred
+
+
+theQuery = u'{query}'
+# theQuery = 'Spring'
+theQuery = urllib.quote_plus(theQuery)
+url = 'http://search.maven.org/solrsearch/select?q=%s&rows=20&wt=json' % theQuery
+
+request = urllib2.Request(url)
+req_open = urllib2.build_opener()
+conn = req_open.open(request)
+req_data = conn.read()
+
+data = json.loads(req_data)
+
+results = []
+
+response = data['response']
+
+for doc in response['response']:
+	arg = doc['id']
+	group = doc['g']
+	artifact = doc['a']
+	version = doc['latestVersion']
+
+	zb = "%s:%s:%s" % (group, artifact, version)
+
+	item = alfred.Item({'uid': 1, 'arg': arg}, group, zb)
+	results.append(item)
+
+xml = alfred.xml(results)
+alfred.write(xml)
